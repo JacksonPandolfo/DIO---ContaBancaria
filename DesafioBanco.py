377de69b4d6f55bfdf1c31a3adf68a7e46821a68
Jackson Pandolfo <jacksonpandolfo@hotmail.com>
2024-01-25 16:30:34 -0300

    Inclusão dos métodos de criação de usuário e conta

--- DesafioBanco.py
+++ DesafioBanco.py
@@ -1,18 +1,15 @@
-menu = """
-
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+import textwrap
+menu = """\n
+================= MENU =================
+[CU]\tCadastrar usuário
+[NC]\tNova conta
+[D]\tDepositar
+[S]\tSacar
+[E]\tExtrato
+[LC]\tListar contas
+[Q]\tSair
 
 => """
-global saldo
-saldo: float = 0.0
-limite = 500
-extratoConta = []
-global numeroSaques
-numeroSaques = 0
-LIMITE_SAQUES = 3
 
 def depositar() -> float:
   global saldo
@@ -68,33 +65,148 @@ def extrato(transacao, valorTransacao, saldo):
 
   extratoConta.append(dataTransaction)
 
+def criarUsuario(usuarios):
+  # usuarioExiste = False
+  
+  usuario = {
+    "nome": None,
+    "sobrenome": None,
+    "dataNascimento":None,
+    "CPF":None,
+    "endereco":{
+      "logradouro": None,
+      "numero": None,
+      "bairro": None,
+      "cidade": None,
+      "siglaEstado": None
+    },
+  }
+  cpf = input("Por favor, digite seu CPF:")
+  usuarioExistente = filtrarUsuario(cpf, usuarios)
 
+  if usuarioExistente:
+    print("Usuário já cadastrado!")
+    return
+  
+  usuario["CPF"] = cpf
+  usuario["nome"] = input("Primeiro nome: ").upper()
+  usuario["sobrenome"] = input("Sobrenome: ").upper()
+  usuario["dataNascimento"] = input("Data de nascimento (DD/MM/AAAA): ")
+  usuario["endereco"]["logradouro"] = input("Logradouro: ").upper()
+  usuario["endereco"]["numero"] = input("Número da residencia: ")
+  usuario["endereco"]["bairro"] = input("Bairro: ").upper()
+  usuario["endereco"]["cidade"] = input("Cidade: ").upper()
+  usuario["endereco"]["siglaEstado"] = input("Estado (UF): ").upper()
+  usuarios.append(usuario)
+
+  # print(len(usuarios))
+  # if len(usuarios) == 0:
+  #   usuario["CPF"] = input("CPF: ")
+  #   usuario["nome"] = input("Primeiro nome: ").upper()
+  #   usuario["sobrenome"] = input("Sobrenome: ").upper()
+  #   usuario["dataNascimento"] = input("Data de nascimento (DD/MM/AAAA): ")
+  #   usuario["endereco"]["logradouro"] = input("Logradouro: ").upper()
+  #   usuario["endereco"]["numero"] = input("Número da residencia: ")
+  #   usuario["endereco"]["bairro"] = input("Bairro: ").upper()
+  #   usuario["endereco"]["cidade"] = input("Cidade: ").upper()
+  #   usuario["endereco"]["siglaEstado"] = input("Estado (UF): ").upper()
+  #   usuarios.append(usuario)
+  # else:
+  #   cpf = input("CPF: ")
+  #   for elm in usuarios:
+  #     if cpf in elm.get("CPF"):
+  #       print("CPF já cadastrado!")
+  #       usuarioExiste = True
+  #       break
+  #   if not usuarioExiste:
+  #     usuario["CPF"] = cpf
+  #     usuario["nome"] = input("Primeiro nome: ").upper()
+  #     usuario["sobrenome"] = input("Sobrenome: ").upper()
+  #     usuario["dataNascimento"] = input("Data de nascimento (DD/MM/AAAA): ")
+  #     usuario["endereco"]["logradouro"] = input("Logradouro: ").upper()
+  #     usuario["endereco"]["numero"] = input("Número da residencia: ")
+  #     usuario["endereco"]["bairro"] = input("Bairro: ").upper()
+  #     usuario["endereco"]["cidade"] = input("Cidade: ").upper()
+  #     usuario["endereco"]["siglaEstado"] = input("Estado (UF): ").upper()
+  #     usuarios.append(usuario)
+
+def filtrarUsuario(cpf, usuarios):
+  usuariosFiltrados = [usuario for usuario in usuarios if usuario["CPF"] == cpf]
+  return usuariosFiltrados[0] if usuariosFiltrados else None
+
+def criarContaCorrente(AGENCIA, numeroConta, usuarios):
+  cpf = input("Digite aqui seu CPF (somente os números):")
+  usuario = filtrarUsuario(cpf, usuarios)
+
+  if usuario:
+    conta = {
+      "agencia": AGENCIA,
+      "numeroConta": numeroConta,
+      "usuario": usuario
+    }
+    print("### Conta criada com sucesso! ###")
+    return conta
+  print("### Cliente não cadastrado, você será redirecionado(a) ao menu! ###")
+
+def listarContas(contas):
+  for conta in contas:
+    linha = f"""\
+      Agência:\t{conta['agencia']}
+      C/C:\t\t{conta['numeroConta']}
+      Titular:\t{conta['usuario']['nome'] + " " + conta['usuario']['sobrenome'] }
+    """
+    print("=" * 100)
+    print(textwrap.dedent(linha))
+  
 if __name__ == "__main__":
+  
+  LIMITE_SAQUES = 3
+  AGENCIA = '0001'
+
+  saldo: float = 0.0
+  limite = 500
+  numeroSaques = 0
+  extratoConta = []
+  usuarios = []
+  contas = []
+
   while True:
+    opcao = input(textwrap.dedent(menu)).upper()
 
-    opcao = input(menu)
+    if opcao == "CU":
+      criarUsuario(usuarios)
 
-    if opcao == "d":
+    elif opcao == "NC":
+      numeroConta = len(contas) + 1
+      conta = criarContaCorrente(AGENCIA, numeroConta, usuarios)
+
+      if conta:
+        contas.append(conta)
+
+    elif opcao == "D":
       depositar()
 
-    elif opcao == "s":
+    elif opcao == "S":
       sacar()
     
-    elif opcao == "e":
+    elif opcao == "E":
       if len(extratoConta) == 0: 
         print("Não foram realizadas movimentações")
       else:
-        for element in extratoConta:
-          for elm in element.keys():
-            if elm == 'Saldo':
-              print()
-              print(f"Saldo:            {element[elm]}")
-            else:
-              print(f"{element[elm]:15}", end=" ")
+        for extrato in extratoConta:
+          linha = f"""\
+            Valor:\t{extrato['Valor']}
+            Saldo:\t  {extrato['Saldo']}
+          """
+          print("=" * 100)
+          print(textwrap.dedent(linha))
     
-    elif opcao == "q":
+    elif opcao == "Q":
       print("Saindo...")
       break
     
+    elif opcao == "LC":
+      listarContas(contas)  
+
     else:
       print("Operação inválida, por favor selecione novamente a opção desejada.")
